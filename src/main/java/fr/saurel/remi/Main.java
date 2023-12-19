7c34e0b59e457cf35a08bf8583ede21df5e5a44c
RemiSaurel <r.saurel5@gmail.com>
2023-12-19 22:16:31 +0100

    add: main input logic

--- src/main/java/fr/saurel/remi/Main.java
+++ src/main/java/fr/saurel/remi/Main.java
@@ -1,10 +1,35 @@
 package fr.saurel.remi;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("Player 1");
-        Player player2 = new Player("Player 2");
         Board board = new Board();
-        Game game = new Game(board, player1, player2);
+        Game game = new Game(board, player1);
+
+        do {
+            game.start();
+            System.out.println("Enter the letters: ");
+            // Wait for user input
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            // Check if input is valid (6 letters)
+            if (input.length() != 6) {
+                System.out.println("You must enter 6 letters");
+                continue;
+            }
+            // Find a word with the 6 letters
+            List<String> possibleWordList = Board.findPossibleWordList(input);
+            if (possibleWordList.isEmpty()) {
+                System.out.println("No words found");
+                continue;
+            } else {
+                System.out.println("Possible words: " + possibleWordList);
+            }
+
+            game.end();
+        } while (true);
     }
 }
