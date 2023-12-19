cd0d1905f51e6d2bb4090be42ecfe4bdf1c5e694
RemiSaurel <r.saurel5@gmail.com>
2023-12-19 22:16:39 +0100

    add: first algos (wip)

--- src/main/java/fr/saurel/remi/Board.java
+++ src/main/java/fr/saurel/remi/Board.java
@@ -1,6 +1,9 @@
 package fr.saurel.remi;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Board {
     private HashMap<Character, Integer> LETTERS_OCCURENCES = new HashMap<Character, Integer>() {{
@@ -47,5 +50,90 @@ public class Board {
 
     public void setLettersOccurences(char letter, int occurences) {
         LETTERS_OCCURENCES.put(letter, occurences);
+    }
+
+    /**
+     * Find the words that can be created with the input letters
+     * @param input The input letters
+     * @return A list of words that can be created with the input letters, or an empty list if no words were found
+     */
+    public static List<String> findPossibleWordList(String input) {
+        List<String> possibleWordList = new ArrayList<>();
+
+        // Use a HashSet for constant-time lookups
+        Set<Character> inputLetters = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            inputLetters.add(c);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/data.txt"))) {
+            // Read all lines at once for efficiency
+            List<String> lines = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            // Sort the words based on the number of letters used
+            lines.sort(Comparator.comparingInt(word -> countUsedLetters(inputLetters, word)));
+
+            // Find words that can be created with the input letters
+            for (String word : lines) {
+                if (canCreateWord(inputLetters, word)) {
+                    possibleWordList.add(word);
+                    // Remove letters from the input letters so they can't be used twice
+                    for (char c : word.toCharArray()) {
+                        inputLetters.remove(c);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return possibleWordList;
+    }
+
+    /**
+     * Count the number of letters in the word that are in the input
+     * @param inputLetters The input letters
+     * @param word The word to check
+     * @return The number of letters in the word that are in the input
+     */
+    private static int countUsedLetters(Set<Character> inputLetters, String word) {
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            if (inputLetters.contains(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Check if a word can be created with the input letters
+     * @param inputLetters The input letters
+     * @param word The word to check
+     * @return True if the word can be created with the input letters, false otherwise
+     */
+    private static boolean canCreateWord(Set<Character> inputLetters, String word) {
+        // Use a Map to track the frequency of each letter in the word
+        Map<Character, Integer> wordFrequency = new HashMap<>();
+
+        for (char c : word.toCharArray()) {
+            wordFrequency.put(c, wordFrequency.getOrDefault(c, 0) + 1);
+        }
+
+        // Check if all letters of the word are in the input and have the correct frequency
+        for (Map.Entry<Character, Integer> entry : wordFrequency.entrySet()) {
+            char letter = entry.getKey();
+            int frequency = entry.getValue();
+
+            if (!inputLetters.contains(letter) || inputLetters.stream().filter(c -> c == letter).count() < frequency) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
