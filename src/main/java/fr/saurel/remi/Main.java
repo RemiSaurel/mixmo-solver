9abc7f06fe5ee960e33f49548521f3ff7cd9c58b
RemiSaurel <r.saurel5@gmail.com>
2023-12-20 19:09:26 +0100

    add: main strategy

--- src/main/java/fr/saurel/remi/Main.java
+++ src/main/java/fr/saurel/remi/Main.java
@@ -1,35 +1,60 @@
 package fr.saurel.remi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Player 1");
         Board board = new Board();
-        Game game = new Game(board, player1);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of players: ");
+        int nbPlayers = scanner.nextInt();
 
+        // Create players
+        Player[] players = new Player[nbPlayers];
+        for (int i = 0; i < nbPlayers; i++) {
+            System.out.println("Enter player " + (i + 1) + " name: ");
+            String name = scanner.next();
+            players[i] = new Player(name);
+        }
+
+        // Create game
+        Game game = new Game(board, players);
+        game.setPlayers(new ArrayList<>(Arrays.asList(players)));
+        game.start();
         do {
-            game.start();
-            System.out.println("Enter the letters: ");
-            // Wait for user input
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            // Check if input is valid (6 letters)
-            if (input.length() != 6) {
-                System.out.println("You must enter 6 letters");
-                continue;
-            }
-            // Find a word with the 6 letters
-            List<String> possibleWordList = Board.findPossibleWordList(input);
-            if (possibleWordList.isEmpty()) {
-                System.out.println("No words found");
-                continue;
-            } else {
-                System.out.println("Possible words: " + possibleWordList);
+            for (Player player : game.getPlayers()) {
+                System.out.println(player.getName() + " turn:");
+                // Wait for user input
+                Scanner sc = new Scanner(System.in);
+                String input = sc.nextLine();
+
+                // Check if input is valid
+                if (game.isInputInvalid(input)) {
+                    System.out.println("You must enter " + (game.isFirstTurn() ? "6" : "2") + " letters");
+                    continue;
+                }
+
+                ArrayList<Character> letters = new ArrayList<>();
+                for (char c : input.toCharArray()) {
+                    letters.add(c);
+                }
+
+                player.addLetters(letters);
+
+                // Find a word with the 6 letters
+                List<String> possibleWordList = Board.findPossibleWordList(player.getLetters());
+                if (possibleWordList.isEmpty()) {
+                    System.out.println("No words found");
+                } else {
+                    System.out.println("Possible words: " + possibleWordList);
+                }
             }
 
-            game.end();
+            // Set the first turn to false after first turn
+            game.setFirstTurn(false);
         } while (true);
     }
 }
